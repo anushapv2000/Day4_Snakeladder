67b61ca490d67b86fa0c1eb79fd430e3af13b512
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-23 20:54:00 +0530

    option

--- bridgelabz/SnakeLadder.java
+++ bridgelabz/SnakeLadder.java
@@ -3,16 +3,34 @@ package bridgelabz;
 import java.util.Random;
 
 public class SnakeLadder {
+    static int position=0;
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Game");
-        int position=0;
+
+        int dice=0;
         System.out.println("Game Started with position "+position);
-        rolls();
+        dice=rolls();
+        System.out.println(option(dice,"Ladder"));
     }
-    public static void rolls()
+    public static int rolls()
     {
         Random random=new Random();
-        System.out.println(random.nextInt(6)+1);
+        return random.nextInt(6)+1;
+    }
+    public static int option(int dice,String opt){
+        if (opt=="No Play")
+        {
+            return position;
+        } else if (opt=="Ladder") {
+            position=position+dice;
+            return position;
+        }
+        else
+        {
+            position=position-dice;
+            return position;
+        }
+
     }
 }
