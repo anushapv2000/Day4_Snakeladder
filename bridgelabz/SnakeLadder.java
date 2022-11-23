a7fbfbb4b524f38bd6d5d658e73e255e704c16be
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-23 20:42:07 +0530

    Roll DIce

--- bridgelabz/SnakeLadder.java
+++ bridgelabz/SnakeLadder.java
@@ -1,10 +1,18 @@
 package bridgelabz;
 
+import java.util.Random;
+
 public class SnakeLadder {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Game");
         int position=0;
         System.out.println("Game Started with position "+position);
+        rolls();
+    }
+    public static void rolls()
+    {
+        Random random=new Random();
+        System.out.println(random.nextInt(6)+1);
     }
 }
