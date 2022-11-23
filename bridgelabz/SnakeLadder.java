d32603fca10f9c41dfdb8f89442a1518326515f6
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-23 20:33:02 +0530

    firstposition

---
+++ bridgelabz/SnakeLadder.java
@@ -0,0 +1,10 @@
+package bridgelabz;
+
+public class SnakeLadder {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the Game");
+        int position=0;
+        System.out.println("Game Started with position "+position);
+    }
+}
