19f830cbf30c179358fa43b86ca477e0cad499fc
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-23 22:09:53 +0530

    player2

--- bridgelabz/SnakeLadder.java
+++ bridgelabz/SnakeLadder.java
@@ -3,25 +3,40 @@ package bridgelabz;
 import java.util.Random;
 
 public class SnakeLadder {
-    static int position = 0;
-
+    static int position1 = 0,position2=0;
+    static String opt[] = {"No Play", "Ladder", "Snake"};
     public static void main(String[] args) {
         System.out.println("Welcome to the Game");
-        System.out.println("Game Started with position " + position);
+        System.out.println("Game Started with position " + position1);
         till100();
         //System.out.println(option(dice));
     }
     public static void till100() {
         Random random = new Random();
-        int dice = 0;
+        int dice1 = 0,dice2=0;
         int c=0;
-        String opt[] = {"No Play", "Ladder", "Snake"};
-        while (position < 100) {
-            dice = rolls();
+
+        while (position1 < 100 && position2<100 ) {
+            dice1 = rolls();
+            dice2 = rolls();
             c=c+1;
-            int a = random.nextInt(3);
-            System.out.println("Position"+option(dice, opt[a]));
-        }
+            int a1 = random.nextInt(3);
+            int a2 = random.nextInt(3);
+            position1=option(dice1,opt[a1],position1 );
+            position2=option(dice2, opt[a2],position2);
+            System.out.println("Position1 :"+position1);
+            System.out.println("Position2 :"+position2);
+            if(position2==100){
+                System.out.println("Player 2 Wins");
+                break;
+            } else if (position1==100) {
+                System.out.println("Player 1 Wins");
+                break;
+            }
+            else {continue;}
+
+            }
+
         System.out.println("number of times the dice was thrown "+c);
 
     }
@@ -34,21 +49,25 @@ public class SnakeLadder {
 
     }
 
-    public static int option(int dice, String opt) {
-        if (opt == "No Play")
+    public static int option(int dice, String opval,int position) {
+        if (opval == "No Play")
         {
             position=position;
         }
-        else if (opt == "Ladder")
+        else if (opval == "Ladder")
         {
 
-            //return position;
             if((position+dice)>100)
             {
                 position=position;
             }
             else {
                 position = position + dice;
+                Random random = new Random();
+                int d=rolls();
+                int o = random.nextInt(3);
+                position=option(d,opt[o],position );
+
             }
         }
         else
